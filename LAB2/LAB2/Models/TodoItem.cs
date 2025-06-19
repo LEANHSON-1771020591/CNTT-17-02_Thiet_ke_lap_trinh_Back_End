5ad856fe38367233f641aaa0fb7413ca0b31c502
LE ANH SON <1771020591@dnu.edu.vn>
2025-06-19 09:03:22 +0700

    Lần đầu gửi bài lên GitHub

---
+++ LAB2/LAB2/Models/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LAB2.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Task không được để trống")]
+        [StringLength(100, ErrorMessage = "Task không được vượt quá 100 ký tự")]
+        public string Task { get; set; } = string.Empty;
+    }
+}
